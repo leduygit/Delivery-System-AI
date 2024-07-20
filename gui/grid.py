9a0cef842a7c4d4a70b6f15567a642dbeb73b0b9
KHONGPHAITKID <hctuankiet243@gmail.com>
2024-07-20 17:46:28 +0700

    feat: scaling screen base on map

--- gui/grid.py
+++ gui/grid.py
@@ -1,6 +1,7 @@
 # grid.py
 import pygame
-from config import WHITE, GRAY, BLACK, GRID_SIZE, GRID_WIDTH, GRID_HEIGHT, font
+import config
+from config import *
 
 class Grid:
     def __init__(self, grid_data, offset=(0, 0)):
@@ -9,12 +10,9 @@ class Grid:
 
     def draw(self, screen):
         screen.fill(WHITE)
-        for row in range(GRID_HEIGHT):
-            for col in range(GRID_WIDTH):
-                try:
-                    cell_value = str(self.grid_data[row][col])
-                except:
-                    cell_value = '0'
+        for row in range(len(self.grid_data)):
+            for col in range(len(self.grid_data[row])):
+                cell_value = str(self.grid_data[row][col])
                 color = WHITE
                 text_color = BLACK
                 border_color = BLACK
@@ -22,20 +20,16 @@ class Grid:
                 if cell_value[0] == 'S':
                     color = GRAY  # Start positions
                 elif cell_value[0] == 'G':
-                    border_color = GRAY  # Goals
+                    color = PLAYER_COLORS[cell_value[1]]  # Goals
                 elif cell_value == 'F':
                     color = GRAY  # Fuel stations
                 elif cell_value == '-1':
                     color = BLACK  # Obstacles
-                elif cell_value != '0':
-                    border_color = BLACK
 
-                pygame.draw.rect(screen, color, (col * GRID_SIZE + self.offset[0], row * GRID_SIZE + self.offset[1], GRID_SIZE, GRID_SIZE))
-                pygame.draw.rect(screen, border_color, (col * GRID_SIZE + self.offset[0], row * GRID_SIZE + self.offset[1], GRID_SIZE, GRID_SIZE), 1)
+                pygame.draw.rect(screen, color, (col * config.GRID_SIZE + self.offset[0], row * config.GRID_SIZE + self.offset[1], config.GRID_SIZE, config.GRID_SIZE))
+                pygame.draw.rect(screen, border_color, (col * config.GRID_SIZE + self.offset[0], row * config.GRID_SIZE + self.offset[1], config.GRID_SIZE, config.GRID_SIZE), 2)
 
                 if cell_value not in ['-1', '0']:
                     text_surface = font.render(cell_value, True, text_color)
-                    text_rect = text_surface.get_rect(center=(col * GRID_SIZE + GRID_SIZE // 2 + self.offset[0], row * GRID_SIZE + GRID_SIZE // 2 + self.offset[1]))
+                    text_rect = text_surface.get_rect(center=(col * config.GRID_SIZE + config.GRID_SIZE // 2 + self.offset[0], row * config.GRID_SIZE + config.GRID_SIZE // 2 + self.offset[1]))
                     screen.blit(text_surface, text_rect)
-
-        pygame.draw.rect(screen, BLACK, (self.offset[0], self.offset[1], GRID_WIDTH * GRID_SIZE, GRID_HEIGHT * GRID_SIZE), 3)
--- gui/visualizer.py
+++ gui/visualizer.py
@@ -1,6 +1,7 @@
 # visualizer.py
 import pygame
 import json
+import config
 from config import *
 from grid import Grid
 from player import Player
@@ -9,48 +10,39 @@ from sidebar import Sidebar
 class Visualizer:
     def __init__(self, FILENAME):
         self.screen = pygame.display.set_mode(WINDOW_SIZE)
-        pygame.display.set_caption('Delivery-System-AI')
+        pygame.display.set_caption('Multi-player Grid Visualization')
         self.state = self.load_state(FILENAME)
-        self.state_ids = list(self.state.keys())
-        # self.state_ids = self.state["moves"]
-        self.current_state_index = 0
+        self.current_turn_index = 0
         self.playing = False
         self.frame_count = 0
         self.clock = pygame.time.Clock()
+        self.update_grid_size()
+        self.offset = ((WINDOW_WIDTH - config.GRID_SIZE * len(self.state["map"][0])) / 2, (WINDOW_HEIGHT - config.GRID_SIZE * len(self.state["map"])) / 2)
+        print(self.offset)
         self.buttons = {
-            'previous': pygame.Rect(GRID_WIDTH * GRID_SIZE + 10, GRID_HEIGHT * GRID_SIZE - 3 * BUTTON_HEIGHT - 20, SIDEBAR_WIDTH - 20, BUTTON_HEIGHT),
-            'next': pygame.Rect(GRID_WIDTH * GRID_SIZE + 10, GRID_HEIGHT * GRID_SIZE - 2 * BUTTON_HEIGHT - 20, SIDEBAR_WIDTH - 20, BUTTON_HEIGHT),
-            'play_stop': pygame.Rect(GRID_WIDTH * GRID_SIZE + 10, GRID_HEIGHT * GRID_SIZE - BUTTON_HEIGHT - 20, SIDEBAR_WIDTH - 20, BUTTON_HEIGHT),
+            'previous': pygame.Rect(0, 0, SIDEBAR_WIDTH - 20, BUTTON_HEIGHT),
+            'next': pygame.Rect(0, 0, SIDEBAR_WIDTH - 20, BUTTON_HEIGHT),
+            'play_stop': pygame.Rect(0, 0, SIDEBAR_WIDTH - 20, BUTTON_HEIGHT),
             'playing': False
         }
-        self.offset = (WINDOW_WIDTH // 2 - GRID_WIDTH * GRID_SIZE // 2, WINDOW_HEIGHT // 2 - GRID_HEIGHT * GRID_SIZE // 2)
-        self.grid = Grid(self.get_current_grid(), self.offset)
-        self.player = Player(self.offset)
-        self.sidebar = Sidebar(self.buttons, self.player.player_images, self.offset)
+        self.grid = Grid(self.state['map'], offset=self.offset)
+        self.player = Player(offset=self.offset)
+        self.sidebar = Sidebar(self.buttons, self.player.player_images)
 
-    def handleSize(self):
-        # map = self.state["map"]
-        # row = len(map)
-        # col = len(map[0])
-        # GRID_SIZE = min(WINDOW_WIDTH // col, WINDOW_HEIGHT // row, 30)
-        # GRID_WIDTH = col
-        # GRID_HEIGHT = row
-        pass
-
-    def changeFile(self, FILENAME):
-        self.state = self.load_state(FILENAME)
-        self.state_ids = list(self.state.keys())
+    def update_grid_size(self):
+        row = len(self.state["map"])
+        col = len(self.state["map"][0])
+        config.GRID_SIZE = min(WINDOW_WIDTH // col, WINDOW_HEIGHT // row)
+        ratio = config.GRID_SIZE / 30
+        config.PLAYER_IMAGE_SIZE = (config.PLAYER_IMAGE_SIZE[0] * ratio, config.PLAYER_IMAGE_SIZE[1] * ratio)
 
     def load_state(self, filename):
         with open(filename, 'r') as f:
             return json.load(f)
 
-    def get_current_grid(self):
-        return [row.split() for row in self.state[self.state_ids[self.current_state_index]]['map']['grid']]
-    
-    def get_players(self):
-        return {k: v for k, v in self.state[self.state_ids[self.current_state_index]].items() if k.startswith('player')}
-    
+    def get_current_turn(self):
+        return self.state['moves'][self.current_turn_index][f'turn {self.current_turn_index}']
+
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -58,10 +50,10 @@ class Visualizer:
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = event.pos
                 if self.buttons['previous'].collidepoint(mouse_pos):
-                    self.current_state_index = max(0, self.current_state_index - 1)
+                    self.current_turn_index = max(0, self.current_turn_index - 1)
                     self.buttons['playing'] = False
                 elif self.buttons['next'].collidepoint(mouse_pos):
-                    self.current_state_index = min(len(self.state_ids) - 1, self.current_state_index + 1)
+                    self.current_turn_index = min(len(self.state['moves']) - 1, self.current_turn_index + 1)
                     self.buttons['playing'] = False
                 elif self.buttons['play_stop'].collidepoint(mouse_pos):
                     self.buttons['playing'] = not self.buttons['playing']
@@ -69,14 +61,14 @@ class Visualizer:
 
     def update_state(self):
         if self.buttons['playing'] and self.frame_count % STATE_DELAY == 0:
-            self.current_state_index = (self.current_state_index + 1) % len(self.state_ids)
-            if self.current_state_index == len(self.state_ids) - 1:
+            self.current_turn_index = (self.current_turn_index + 1) % len(self.state['moves'])
+            if self.current_turn_index == len(self.state['moves']) - 1:
                 self.buttons['playing'] = False
 
     def draw(self):
         self.grid.draw(self.screen)
-        self.player.draw(self.screen, self.get_players(), self.frame_count)
-        self.sidebar.draw(self.screen, self.get_players(), self.state_ids[self.current_state_index])
+        self.player.draw(self.screen, self.get_current_turn(), self.frame_count)
+        self.sidebar.draw(self.screen, self.get_current_turn(), self.current_turn_index)
         pygame.display.flip()
 
     def run(self):
@@ -88,6 +80,6 @@ class Visualizer:
         pygame.quit()
 
 if __name__ == "__main__":
-    FILENAME = '../Assets/Json/state.json'
+    FILENAME = "../Assets/Json/output.json"
     visualizer = Visualizer(FILENAME)
     visualizer.run()
