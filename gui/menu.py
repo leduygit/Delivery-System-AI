12936f5db6f6e3a3cf73741e3015241dc561f1df
leduygit <leduyanh04@gmail.com>
2024-07-21 12:23:18 +0700

    feat: add menu screen

---
+++ gui/menu.py
@@ -0,0 +1,34 @@
+import pygame
+from config import WINDOW_SIZE
+
+class Menu:
+    def __init__(self):
+        # Load menu images
+        self.background = pygame.image.load('../assets/images/menu/background.png')
+        self.start_button = pygame.image.load('../assets/images/menu/lv1.png')
+        self.exit_button = pygame.image.load('../assets/images/menu/exit.png')
+
+        # Scale the menu background to fit the window size
+        self.background = pygame.transform.scale(self.background, WINDOW_SIZE)
+
+        # Button rectangles for detecting clicks
+        self.start_button_rect = self.start_button.get_rect(center=(WINDOW_SIZE[0] // 2, WINDOW_SIZE[1] // 2 - 50))
+        self.exit_button_rect = self.exit_button.get_rect(center=(WINDOW_SIZE[0] // 2, WINDOW_SIZE[1] // 2 + 50))
+
+    def draw(self, screen):
+        screen.blit(self.background, (0, 0))
+        screen.blit(self.start_button, self.start_button_rect.topleft)
+        screen.blit(self.exit_button, self.exit_button_rect.topleft)
+        pygame.display.flip()
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return 'exit'
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos
+                if self.start_button_rect.collidepoint(mouse_pos):
+                    return 'start'
+                elif self.exit_button_rect.collidepoint(mouse_pos):
+                    return 'exit'
+        return None
--- gui/visualizer.py
+++ gui/visualizer.py
@@ -1,10 +1,10 @@
-# visualizer.py
 import pygame
 import json
 from config import *
 from grid import Grid
 from player import Player
 from sidebar import Sidebar
+from menu import Menu  # Import the Menu class
 
 class Visualizer:
     def __init__(self, FILENAME):
@@ -16,12 +16,20 @@ class Visualizer:
         self.playing = False
         self.frame_count = 0
         self.clock = pygame.time.Clock()
+
+        # Initialize buttons dictionary
         self.buttons = {
             'previous': pygame.Rect(GRID_WIDTH * GRID_SIZE + 10, GRID_HEIGHT * GRID_SIZE - 3 * BUTTON_HEIGHT - 20, SIDEBAR_WIDTH - 20, BUTTON_HEIGHT),
             'next': pygame.Rect(GRID_WIDTH * GRID_SIZE + 10, GRID_HEIGHT * GRID_SIZE - 2 * BUTTON_HEIGHT - 20, SIDEBAR_WIDTH - 20, BUTTON_HEIGHT),
             'play_stop': pygame.Rect(GRID_WIDTH * GRID_SIZE + 10, GRID_HEIGHT * GRID_SIZE - BUTTON_HEIGHT - 20, SIDEBAR_WIDTH - 20, BUTTON_HEIGHT),
             'playing': False
         }
+
+        # Initialize Menu
+        self.menu = Menu()
+        self.menu_active = True
+
+        # Initialize Grid, Player, and Sidebar
         self.grid = Grid(self.get_current_grid())
         self.player = Player()
         self.sidebar = Sidebar(self.buttons)
@@ -39,21 +47,32 @@ class Visualizer:
 
     def get_players(self):
         return {k: v for k, v in self.state[self.state_ids[self.current_state_index]].items() if k.startswith('player')}
-    
+
     def handle_events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        if self.menu_active:
+            result = self.menu.handle_events()
+            if result == 'exit':
                 return False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = event.pos
-                if self.buttons['previous'].collidepoint(mouse_pos):
-                    self.current_state_index = max(0, self.current_state_index - 1)
-                    self.buttons['playing'] = False
-                elif self.buttons['next'].collidepoint(mouse_pos):
-                    self.current_state_index = min(len(self.state_ids) - 1, self.current_state_index + 1)
-                    self.buttons['playing'] = False
-                elif self.buttons['play_stop'].collidepoint(mouse_pos):
-                    self.buttons['playing'] = not self.buttons['playing']
+            elif result == 'start':
+                self.menu_active = False
+                return True
+        else:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = event.pos
+                    if self.buttons['previous'].collidepoint(mouse_pos):
+                        self.current_state_index = max(0, self.current_state_index - 1)
+                        self.buttons['playing'] = False
+                    elif self.buttons['next'].collidepoint(mouse_pos):
+                        self.current_state_index = min(len(self.state_ids) - 1, self.current_state_index + 1)
+                        self.buttons['playing'] = False
+                    elif self.buttons['play_stop'].collidepoint(mouse_pos):
+                        self.buttons['playing'] = not self.buttons['playing']
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:  # Check if Escape key is pressed
+                        self.menu_active = True  # Return to menu
         return True
 
     def update_state(self):
@@ -70,8 +89,11 @@ class Visualizer:
 
     def run(self):
         while self.handle_events():
-            self.update_state()
-            self.draw()
+            if self.menu_active:
+                self.menu.draw(self.screen)
+            else:
+                self.update_state()
+                self.draw()
             self.frame_count += 1
             self.clock.tick(60)
         pygame.quit()
