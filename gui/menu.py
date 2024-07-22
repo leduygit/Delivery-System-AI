d876e2d62fa286f0ca051d39fa67233239f8cc55
leduygit <leduyanh04@gmail.com>
2024-07-23 02:05:27 +0700

    refactor: change start button to level button

--- gui/menu.py
+++ gui/menu.py
@@ -5,14 +5,14 @@ class Menu:
     def __init__(self):
         # Load menu images
         self.background = pygame.image.load('Assets/images/menu/background.png')
-        self.start_button = pygame.image.load('Assets/images/menu/lv1.png')
+        self.lv1_button = pygame.image.load('Assets/images/menu/lv1.png')
         self.lv2_button = pygame.image.load('Assets/images/menu/lv2.png')
         self.lv3_button = pygame.image.load('Assets/images/menu/lv3.png')
         self.lv4_button = pygame.image.load('Assets/images/menu/lv4.png')
         self.exit_button = pygame.image.load('Assets/images/menu/exit.png')
 
         # Load hover images
-        self.start_button_hover = pygame.image.load('Assets/images/menu/lv1-2.png')
+        self.lv1_button_hover = pygame.image.load('Assets/images/menu/lv1-2.png')
         self.lv2_button_hover = pygame.image.load('Assets/images/menu/lv2-2.png')
         self.lv3_button_hover = pygame.image.load('Assets/images/menu/lv3-2.png')
         self.lv4_button_hover = pygame.image.load('Assets/images/menu/lv4-2.png')
@@ -23,7 +23,7 @@ class Menu:
 
         # Button rectangles for detecting clicks
         button_y = WINDOW_SIZE[1] // 2 - 100
-        self.start_button_rect = self.start_button.get_rect(center=(WINDOW_SIZE[0] // 2, button_y))
+        self.lv1_button_rect = self.lv1_button.get_rect(center=(WINDOW_SIZE[0] // 2, button_y))
         self.lv2_button_rect = self.lv2_button.get_rect(center=(WINDOW_SIZE[0] // 2, button_y + 100))
         self.lv3_button_rect = self.lv3_button.get_rect(center=(WINDOW_SIZE[0] // 2, button_y + 200))
         self.lv4_button_rect = self.lv4_button.get_rect(center=(WINDOW_SIZE[0] // 2, button_y + 300))
@@ -35,10 +35,10 @@ class Menu:
         screen.blit(self.background, (0, 0))
         
         # Draw buttons with hover effect
-        if self.hovered_button == 'start':
-            screen.blit(self.start_button_hover, self.start_button_rect.topleft)
+        if self.hovered_button == 'lv1':
+            screen.blit(self.lv1_button_hover, self.lv1_button_rect.topleft)
         else:
-            screen.blit(self.start_button, self.start_button_rect.topleft)
+            screen.blit(self.lv1_button, self.lv1_button_rect.topleft)
 
         if self.hovered_button == 'lv2':
             screen.blit(self.lv2_button_hover, self.lv2_button_rect.topleft)
@@ -74,8 +74,8 @@ class Menu:
 
         # Update hovered button based on mouse position
         if mouse_pos:
-            if self.start_button_rect.collidepoint(mouse_pos):
-                self.hovered_button = 'start'
+            if self.lv1_button_rect.collidepoint(mouse_pos):
+                self.hovered_button = 'lv1'
             elif self.lv2_button_rect.collidepoint(mouse_pos):
                 self.hovered_button = 'lv2'
             elif self.lv3_button_rect.collidepoint(mouse_pos):
@@ -88,7 +88,7 @@ class Menu:
                 self.hovered_button = None
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if self.start_button_rect.collidepoint(mouse_pos):
+                if self.lv1_button_rect.collidepoint(mouse_pos):
                     return "lv1"
                 elif self.lv2_button_rect.collidepoint(mouse_pos):
                     return "lv2"
