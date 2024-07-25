b150f623df5c8886bc3af1c3e460ed8b751e4592
winprn <minhladz001@gmail.com>
2024-07-25 16:13:42 +0700

    merge

--- search_logic/level4.py
+++ search_logic/level4.py
@@ -5,23 +5,44 @@ from search_logic.main import load_data
 import search_logic.format_output as fo
 
 
-def is_valid_move(grid, ox, oy, nx, ny):
+def is_valid_move(grid, state, move):
+    ox, oy = state['x'], state['y']
+    nx, ny = move
+    grid = grid['grid']
     return 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and \
-           grid[nx][ny] != -1 and abs(nx - ox) + abs(ny - oy) == 1
+           grid[nx][ny] != -1 and abs(nx - ox) + abs(ny - oy) == 1 \
+           and state['gas'] > 0 and state['time'] > 0
 
 
-def apply_moves(grid, start_position, move, index):
-    grid[start_position[0]][start_position[1]] = '.'
+def apply_moves(map, state, move, index):
+    cur_x, cur_y = state['x'], state['y']
+    goal = (state['goal_x'], state['goal_y'])
+    grid = map['grid']
+    grid[cur_x][cur_y] = '.'
+    if move == goal:
+        state['goal_x'], state['goal_y'] = get_new_goal(grid, (cur_x, cur_y))
+    if isinstance(grid[move[0]][move[1]], tuple) and grid[move[0]][move[1]].startswith('F'):
+        state['gas'] = map['gas']
+        state['wait'] = int(grid[move[0]][move[1]][1])
+    else:
+        state['gas'] -= 1
+
+    if isinstance(grid[move[0]][move[1]], int):
+        state['wait'] = grid[move[0]][move[1]]
+    
+    state['time'] -= 1
+    state['x'], state['y'] = move
     grid[move[0]][move[1]] = 'S{}'.format(index)
-    return grid
+    return grid, state
 
 
-def print_current(positions, goals):
-    for i, pos in enumerate(positions):
+def print_current(states):
+    for i, _ in enumerate(states):
+        pos = (states[i]['x'], states[i]['y'])
         with open('search_logic/agents/agent_{}.txt'.format(i + 1), 'a') as f:
             f.write('{} {}\n'.format(pos[0], pos[1]))
 
-    for i, goal in enumerate(goals):
+        goal = (states[i]['goal_x'], states[i]['goal_y'])
         with open('search_logic/agents/goal_{}.txt'.format(i + 1), 'a') as f:
             f.write('{} {}\n'.format(goal[0], goal[1]))
 
@@ -49,6 +70,7 @@ def runner():
         'grid': grid,
         'height': len(grid),
         'width': len(grid[0]),
+        'gas': gas,
     }
     # [[(1, 1), (7, 8)], [(2, 5), (9, 0)], [(8, 5), (4, 6)]]
     current_positions = [pos[0] for pos in start_positions]
@@ -56,6 +78,17 @@ def runner():
     print(current_positions)
     print(current_goals)
     bots = [RandomBot() for _ in current_positions]
+    states = []
+    for i, bot in enumerate(bots):
+        states.append({
+            'x': current_positions[i][0],
+            'y': current_positions[i][1],
+            'goal_x': current_goals[i][0],
+            'goal_y': current_goals[i][1],
+            'gas': gas,
+            'time': time,
+            'wait': 0,
+        })
 
     for i, bot in enumerate(bots):
         with open('search_logic/agents/agent_{}.txt'.format(i + 1), 'w') as f:
@@ -65,22 +98,14 @@ def runner():
 
     while time:
         for i, bot in enumerate(bots):
-            state = {
-                'x': current_positions[i][0],
-                'y': current_positions[i][1],
-            }
-            move = bot.get_move(mmap, state)
+            move = bot.get_move(mmap, states[i])
             print(move)
-            if move is None or not is_valid_move(mmap['grid'], 
-                                                    current_positions[i][0], current_positions[i][1],
-                                                    move[0], move[1]):
-                print_current(current_positions, current_goals)
+            if move is None or not is_valid_move(mmap, states[i], move) or states[i]['wait'] > 0:
+                states[i]['wait'] -= states[i]['wait'] > 0
+                print_current(states)
                 continue
-            mmap['grid'] = apply_moves(mmap['grid'], current_positions[i], move, i)
-            if move == current_goals[i]:
-                current_goals[i] = get_new_goal(mmap['grid'], current_positions[i])
-            current_positions[i] = move
-            print_current(current_positions, current_goals)
+            mmap['grid'], states[i] = apply_moves(mmap, states[i], move, i)
+            print_current(states)
         time -= 1
 
     input_files = [f'search_logic/agents/agent_{i+1}.txt' for i in range(len(bots))]
